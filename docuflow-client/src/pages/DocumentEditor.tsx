be9abf53935454235e21d5821d892efca75cd33b
pavneetSingh <pavneets436@gmail.com>
2026-02-17 20:59:53 +0530

    logic to handle fetch Documents

--- docuflow-client/src/pages/DocumentEditor.tsx
+++ docuflow-client/src/pages/DocumentEditor.tsx
@@ -1,17 +1,50 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import api from "../lib/axios";
 
-export const DcoumentEditor = () => {
+export const DocumentEditor = () => {
   const { id } = useParams();
+  const [document, setDocument] = useState<any>(null);
   
-  return
-(  <>
-  <div className="text-white p-8">
-    <h1 className="text-3xl font-bold"> Editing Document </h1>
-    <p className="text-gray-400 mt-2"> ID: { id}</p>
+  useEffect(() => {
+    const getDocument = async () => {
       
-    </div>
+      try {
+        const response = await api.get(`/documents/${id}`)
+        
+        setDocument(response.data)
+        
+        
+      }
+      catch (error) {
+        console.error("Failed to fetch document", error)
+        
+        
+      }
+    }
+    getDocument();
+  }
+    ,
+   
+    [id])
+  
+  if (!document) {
+    return <div className="p-8 text-gray-500 ">loading Document...</div>
+  }
     
-  </>
-)
+  
+  return (
+      <div className="text-white p-8">
+        {/* SUCCESS: We now show the real title from the server! */}
+        <h1 className="text-3xl font-bold">{document.title}</h1>
+        <p className="text-gray-400 mt-2 text-sm italic">Database ID: {id}</p>
+        
+        <div className="mt-10 p-4 border border-gray-700 rounded bg-gray-800">
+           <p className="text-gray-300">
+             {document.content || "This document is empty. Start writing soon!"}
+           </p>
+        </div>
+      </div>
+    );
   
 }
