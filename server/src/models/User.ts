bf0219b14f435d745abafe479264cd95adf99252
pavneetSingh <pavneets436@gmail.com>
2026-03-02 19:37:54 +0530

    Model to handle login request

---
+++ server/src/models/User.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { Schema, model } from "mongoose";
+
+const UserSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+  
+})
+
+export default model('User',UserSchema)
