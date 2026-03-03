d8048a60eba312c5943ead682818250e4caaab5b
pavneetSingh <pavneets436@gmail.com>
2026-03-04 01:19:04 +0530

    Authorization header logic

--- docuflow-client/src/lib/axios.ts
+++ docuflow-client/src/lib/axios.ts
@@ -3,6 +3,19 @@ import axios from "axios";
 // custom instance with configuration
 const api = axios.create({
   baseURL: 'http://localhost:3001'
+});
+
+api.interceptors.request.use((config) => {
+   // check the browser's local memo for the saved token
+  const token = localStorage.getItem('token');
+  
+  // if you see the token then take it to the Authorization header
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  
+  // Let req leave 
+  return config;
 })
 
 export default api;
